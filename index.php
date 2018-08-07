de8c5f51fa4285f42ab3d5ed87272086f8e20b30
Gian Carlos Villalobos <gian@gianko.com>
2018-08-07 10:57:18 -0600

    inital commit

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
  * User: ILG
  * Date: 03/08/2018
  */
+
+
+
 $conn = require_once 'conn/connection.php';
 $sql = "SELECT * FROM VALIDACION_SERIES_MASTERBOX";
 $result = sqlsrv_query($conn, $sql);
@@ -11,3 +14,21 @@ while ($row = sqlsrv_fetch_array($result)) {
      echo $row['FECHA'].' '.$row['IDSUCURSAL'].' '. $row['IDSERIE']. '<br>';
 
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Despacho Camión</title>
+</head>
+<body>
+<form action="conn/action.php" method="post">
+    <input type="date" name="fecha" id="fecha" size="40"><br>
+    <input type="text" name="sucursal" maxlength="4" minlength="4" id="sucursal" size="40"><br>
+    <input type="text" name="masterbox" maxlength="10" minlength="10" id="masterbox" size="40"><br>
+    <button type="submit"></button>
+    </form>
+</body>
+</html>
