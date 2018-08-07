5be187d0dd9ee8b15d3dd010e83ebaec87ca6fdf
Gian Carlos Villalobos <gian@gianko.com>
2018-08-07 13:05:23 -0600

    inital commit

--- index.php
+++ index.php
@@ -25,10 +25,42 @@ while ($row = sqlsrv_fetch_array($result)) {
 </head>
 <body>
 <form action="conn/action.php" method="post">
-    <input type="date" name="fecha" id="fecha" size="40"><br>
+    <input type="date" name="fecha" id="fecha" size="40" autofocus><br>
     <input type="text" name="sucursal" maxlength="4" minlength="4" id="sucursal" size="40"><br>
     <input type="text" name="masterbox" maxlength="10" minlength="10" id="masterbox" size="40"><br>
-    <button type="submit"></button>
+    <input type="number" id="count"  disabled />
     </form>
 </body>
 </html>
+
+<script>
+var inputfecha = document.getElementById("fecha");
+var inputsucursal = document.getElementById("sucursal");
+var inputmasterbox = document.getElementById("masterbox");
+
+inputfecha.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("sucursal").select();
+    }
+});
+
+inputsucursal.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("masterbox").select();
+    }
+});
+
+inputmasterbox.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        let count = parseInt(document.getElementById('count').value);
+        count = count++;
+        console.log(count);
+        document.getElementById("count").value = count;
+        document.getElementById("masterbox").value = '';
+        document.getElementById("masterbox").select();
+    }
+});
+</script>
